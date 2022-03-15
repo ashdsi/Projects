082c955ffcf94684cab6c4c276f4f8705d67e0e0
ashdsi <ashdsi@xyz.com>
2022-03-15 09:18:54 +0000

    Initial commit

---
+++ net6webapi/net6webapi/Controllers/EventsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace net6webapi.Controllers
+{
+    
+    public class EventsController : Controller
+    {
+
+        // GET: EventsController
+        
+        [Route("events")]
+        public ActionResult EventsAPI()
+        {
+            var systeminfo = new sysinfo
+            {
+                OSDescription = System.Runtime.InteropServices.RuntimeInformation.OSDescription,
+                OSArchitecture = System.Runtime.InteropServices.RuntimeInformation.OSArchitecture.ToString(),
+                FrameworkDescription = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription,
+                ProcessArchitecture = System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture.ToString(),
+                RuntimeIdentifier = System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier,
+                Feature = "X"
+            };
+            return Ok(systeminfo);
+
+        }
+
+        [HttpGet]
+        [Route("events/health")]
+        public ActionResult HealthAPI()
+        {
+            return Ok("Healthy");
+
+        }
+
+
+    }
+}
